764c535a3702a4dee4cfba6bcc05f87252afb586
EC2 Default User <ec2-user@ip-172-31-17-123.ap-northeast-1.compute.internal>
2024-07-01 18:04:41 +0000

    1 top画面　途中

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -41,7 +41,7 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # protected
   def after_sign_up_path_for(resource)
     root_path
-  end 
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
@@ -62,9 +62,6 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
-  
-  def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
-  end
 
+  
 end
