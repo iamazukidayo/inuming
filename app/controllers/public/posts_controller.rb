73b77f8872fbbc1ecabd477f5241ef1a2b5177f6
iamazukidayo <xxxxxxmiyu@gmail.com>
2024-07-04 03:47:57 +0000

    1 ヘッダー/投稿編集途中　

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -19,6 +19,25 @@ class Public::PostsController < ApplicationController
     @post.user_id = current_user.id
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+    redirect_to post_path(@post.id)
+    else
+    render :edit
+    end
+  end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end 
+
 
 
 
