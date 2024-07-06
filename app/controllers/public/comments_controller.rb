64bf6705261d7b2f2679d227591c0a28ec979e80
iamazukidayo <xxxxxxmiyu@gmail.com>
2024-07-06 08:13:25 +0000

    1　コメント機能非同期化途中

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -5,13 +5,11 @@ class Public::CommentsController < ApplicationController
     comment = current_user.comments.new(comment_params)
     comment.post_id = @post.id
     comment.save
-    redirect_to post_path(@post)
   end
 
 
   def destroy
     Comment.find(params[:id]).destroy
-    redirect_to post_path(params[:post_id])
   end
 
   private
