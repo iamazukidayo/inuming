c2f09f98ccc3305078cfc47f2e79700b3cf0505b
iamazukidayo <xxxxxxmiyu@gmail.com>
2024-07-11 04:53:45 +0000

    2 会員一覧完成

--- config/routes.rb
+++ config/routes.rb
@@ -44,6 +44,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     get '/' => 'homes#top'
-  end 
+    resources :users, only: [:index, :show, :edit, :update]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
