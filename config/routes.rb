6b7a6f7fa35e0c38e9e61e460166f391b441a8ca
iamazukidayo <xxxxxxmiyu@gmail.com>
2024-07-11 01:22:47 +0000

    2　ログイン機能完了

--- config/routes.rb
+++ config/routes.rb
@@ -42,6 +42,8 @@ Rails.application.routes.draw do
   sessions: "admin/sessions"
 }
 
-
+  namespace :admin do
+    get '/' => 'homes#top'
+  end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
