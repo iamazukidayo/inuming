a56a0bfa619eb0e225b1f0eeae7b9d7855afce80
iamazukidayo <xxxxxxmiyu@gmail.com>
2024-07-13 08:15:49 +0000

    2 コメント一覧　完成

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,2 +1,14 @@
 class Admin::CommentsController < ApplicationController
+
+  def index
+    @comments = Comment.includes(:post, :user).all
+    @users = User.all
+  end
+
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to admin_comments_path
+  end
 end
