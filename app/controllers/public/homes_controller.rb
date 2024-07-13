792619ab59dfca8ac507af71838f8e2ceebf4a2e
iamazukidayo <xxxxxxmiyu@gmail.com>
2024-07-13 05:12:50 +0000

    2 会員編集完成

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,8 +1,8 @@
 class Public::HomesController < ApplicationController
-  
+  before_action :authenticate_user!, except: [:top, :about]
   def top
   end
-  
+
   def about
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -38,7 +38,7 @@ Rails.application.routes.draw do
   # devise_for :users
   # root to: 'homes#top'
 
-  devise_for :admin,skip: [:registrations, :passwords], controllers: {
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
   sessions: "admin/sessions"
 }
 
