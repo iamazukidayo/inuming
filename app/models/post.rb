b9d9275755854c016e66e0435e0a1650af339dda
iamazukidayo <xxxxxxmiyu@gmail.com>
2024-07-14 09:49:15 +0000

    1　写真を複数投稿できるようにしました

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,26 +1,17 @@
 class Post < ApplicationRecord
-  has_one_attached :image
+  has_many_attached :images
+  # has_one_attached :images
   belongs_to :user
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
 
   validates :title, presence: true, length: { minimum: 2 }
   validates :body, presence: true, length: { minimum: 10 }
-  validates :image, presence: true
+  validates :images, presence: true
 
 
   def like_by?(user)
     user.present? && likes.exists?(user_id: user.id)
-  end
-
-
-
-  def get_image(width, height)
-      unless image.attached?
-        "no_image.png"
-      else
-        image.variant(resize_to_fill: [width, height]).processed
-      end
   end
 
   def self.liked_posts(user)
@@ -46,5 +37,6 @@ class Post < ApplicationRecord
 
 
 end
+
 
 
