c94e9a5a55c6d1c185ccc41a93f7a6c73eeadc1c
iamazukidayo <xxxxxxmiyu@gmail.com>
2024-07-16 10:26:11 +0000

    1 DM機能実装完了

--- app/controllers/public/messages_controller.rb
+++ app/controllers/public/messages_controller.rb
@@ -2,11 +2,19 @@ class Public::MessagesController < ApplicationController
   before_action :authenticate_user!
   
   def create
-    if Entry.where(:user_id => current_user.id, :room_id => params[:message][:room_id]).present?
-      @mmessage = Message.create(params.require(:message).permit(:message,:user_id, :content, :room_id).merge(:user_id => current_user.id))
-      redirect_to "/rooms/#{@message.room_id}"
+    message = Message.new(message_params)
+    message.user_id = current_user.id
+    if message.save
+      redirect_to room_path(message.room)
     else
-      redirect_back(fallback_location: root_path)
+      redirect_back(faiiback_location: root_path)
     end 
-  end
-end
+  end 
+  
+  private
+  
+  def message_params
+    params.require(:message).permit(:room_id, :message)
+  end 
+end 
+
