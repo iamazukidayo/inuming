d75bc5e034c59652900e2b28eda65ecbb97dc6ce
iamazukidayo <xxxxxxmiyu@gmail.com>
2024-07-18 08:44:03 +0000

    1 通知機能コード編集

--- app/controllers/public/rooms_controller.rb
+++ app/controllers/public/rooms_controller.rb
@@ -4,7 +4,8 @@ class Public::RoomsController < ApplicationController
   def create
     @room = Room.create(user_id: current_user.id)
     @current_entry = Entry.create(user_id: current_user.id, room_id: @room.id)
-    @another_entry = Entry.create(params.require(:entry).permit(:user_id, :room_id).merge(:room_id => @room.id))
+    Rails.logger.debug(params.inspect) # ここでparamsの内容を確認
+    @another_entry = Entry.create(params.require(:entry).permit(:user_id, :room_id).merge(room_id: @room.id))
     redirect_to room_path(@room)
   end
 
