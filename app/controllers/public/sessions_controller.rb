8c1caf5a9605cd96a59533256d022a3599d53d0f
iamazukidayo <xxxxxxmiyu@gmail.com>
2024-07-19 00:31:33 +0000

    1 遷移先の変更/退会済ユーザーのログインフラッシュメッセージ日本語化

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,13 +2,14 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :reject_deleted_user, only: [:create]
 
   def create
     @user = User.find_by(name: params[:user][:name])
     if @user && @user.valid_password?(params[:user][:password])
       sign_in(@user)
       flash[:login] = "ログインしました。"
-      redirect_to posts_path
+      redirect_to user_path(@user)
     else
       flash.now[:alert] = "ユーザー名またはパスワードが間違っています。"
       render :new
@@ -23,10 +24,10 @@ class Public::SessionsController < Devise::SessionsController
 
   protected
 
-  def reject_end_user
-    @end_user = EndUser.find_by(name: params[:end_user][:name])
-    if @end_user
-      if @end_user.valid_password?(params[:end_user][:password]) && (@end_user.is_deleted == true)
+  def reject_deleted_user
+    @user = User.find_by(name: params[:user][:name])
+    if @user
+      if @user.valid_password?(params[:user][:password]) && @user.is_deleted
         flash[:end] = "退会済みです。再度ご登録をしてご利用ください"
         redirect_to new_user_registration_path
       # else
--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -55,7 +55,7 @@ before_action :set_user, only: [:likes]
     @user.update(is_deleted: true)
     reset_session
     flash[:notice] = "退会処理を実行いたしました"
-    redirect_to root_path
+    redirect_to new_user_registration_path
   end
 
 
