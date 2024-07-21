e0fa895dbe50d3b93a53bae9643ce68af78f1f63
iamazukidayo <xxxxxxmiyu@gmail.com>
2024-07-21 09:41:09 +0000

    2 投稿一覧にkaminariつけました

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,6 +1,6 @@
 class Admin::PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.order(created_at: :desc).page(params[:page]).per(10)
     @users = User.all
   end
 
